5d41afb0fa12925eaa10bd0867c8c5e5d72617ac
Sharan Chakradhar <40011522+SirSharanChakradhar@users.noreply.github.com>
2020-05-16 02:21:52 +1000

    appended sql

--- SQL-DBA-SCRIPTS/SQLtoMongo.sql
+++ SQL-DBA-SCRIPTS/SQLtoMongo.sql
@@ -1,15 +1,17 @@
-select count(*) from Customers
-select count(*) from Employees
-select count(*) from Products
-select count(*) from Sales
+--select count(*) from Customers
+--select count(*) from Employees
+--select count(*) from Products
+--select count(*) from Sales
+select SalesPersonID,CustomerID,ProductID,sum(quantity) from Sales
+group by SalesPersonID,CustomerID,ProductID
 
-Select 
+Select top 20
 (Employees.FirstName+' '+Employees.LastName) as Sales_person,
 (Customers.FirstName+' '+Customers.LastName) as Customer_name,
 Products.[Name] as Product_name,
 format(Products.Price,'C','te-IN') as Product_price,
-Sales.Quantity as Quantity,
-format(Sales.Quantity*Products.Price,'C','te-IN') as Total_price
+sum(Sales.Quantity) as Quantity,
+format(sum(Sales.Quantity)*Products.Price,'C','te-IN') as Total_price
 from sales
 inner join Employees
 on Sales.SalesPersonID=Employees.EmployeeID
@@ -17,6 +19,6 @@ inner join Customers
 on  Sales.CustomerID=Customers.CustomerID
 inner join Products
 on Sales.ProductID=Products.ProductID
-group by Products.[Name],Employees.FirstName,Employees.LastName,Sales.Quantity,Products.Price,Customers.FirstName,Customers.LastName
+group by Products.[Name],Employees.FirstName,Employees.LastName,Products.Price,Customers.FirstName,Customers.LastName
 --for json path
-for json auto--, root('sales_report')
+--for json auto--, root('sales_report')
